f794937c0cbace21240b0b89178bbf21450d0709
leonarok <leonard.kielland@gmail.com>
2018-03-16 23:00:06 +0100

    Commented functions

--- gammaderive.m
+++ gammaderive.m
@@ -1,12 +1,22 @@
 function [d_gamma] = gammaderive(h,gamma)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%-----CALCULATES THE DERIVATIVE OF GAMMA BASED ON FINITE DIFFERENCING-----%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 k=length(gamma);
 
+%-----------------------------------------------------------------%
+% Calculate first and last derivative with backwards and forwards %
+% differencing                                                    %
+%-----------------------------------------------------------------%
 d_gamma(1)=(-gamma(3)+4*gamma(2)-3*...
         gamma(1))/(2*h);
 d_gamma(k)=(gamma(k-2)-4*gamma(k-1)+3*...
         gamma(k))/(2*h);
-    
+
+%-------------------------------------------------------%
+% Calculate inner derivatives with central differencing %
+%-------------------------------------------------------%
 for i = 2:k-1
     d_gamma(i)=(-gamma(i-1)+gamma(i+1))/...
         (2*h);
--- percentdone_print.m
+++ percentdone_print.m
@@ -1,4 +1,8 @@
 function [] = percentdone_print(num,num_fin)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%--------PRINTS THE PERCENTAGE COMPLETED TOWARDS CONVERGENCE GOAL---------%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 frac=num_fin/num;
 clc
 fprintf('%6.2f%% completed\n',frac*100)
